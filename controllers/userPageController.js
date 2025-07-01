cf14857a9e1eadad394e6fdc659b4499bfdd86b4
samuel <samawyck@gmail.com>
2025-07-01 10:39:46 -0500

    Hack out userPage handler

---
+++ controllers/userPageController.js
@@ -0,0 +1,15 @@
+const asyncHandler = require("express-async-handler");
+
+
+
+const userPageGet = asyncHandler(async function(req, res) {
+    return res.render("userPage", {
+        docTitle: req.user.username
+    })
+});
+
+
+
+module.exports = {
+    userPageGet
+};
