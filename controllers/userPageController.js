de62799ee007b3f0a9838c52910bfa7a752c4d8f
samuel <samawyck@gmail.com>
2025-07-01 11:31:25 -0500

    Add optional file path params

--- controllers/userPageController.js
+++ controllers/userPageController.js
@@ -3,8 +3,10 @@ const asyncHandler = require("express-async-handler");
 
 
 const userPageGet = asyncHandler(async function(req, res) {
+    const filePath = req.params.path;
+
     return res.render("userPage", {
-        docTitle: req.user.username
+        docTitle: filePath
     });
 });
 
