d4834d31f3d7e071d8dc49dd1b2cee0036bdb768
samuel <samawyck@gmail.com>
2025-07-01 10:54:02 -0500

    add semi-colon

--- controllers/userPageController.js
+++ controllers/userPageController.js
@@ -5,7 +5,7 @@ const asyncHandler = require("express-async-handler");
 const userPageGet = asyncHandler(async function(req, res) {
     return res.render("userPage", {
         docTitle: req.user.username
-    })
+    });
 });
 
 
