56f33f267b659f74e1723ee4bba0d645ab89111e
samuel <samawyck@gmail.com>
2025-07-03 15:44:11 -0500

    Create handler for delete folders

--- controllers/deleteController.js
+++ controllers/deleteController.js
@@ -3,6 +3,7 @@ const db = require("../db/querys.js");
 const supabase = require("../utils/supabaseConfig.js");
 const path = require("node:path");
 const removeFirstDirFromPath = require("../utils/removeFirstDirFromPath.js");
+const getAllFiles = require("../utils/getAllFilesRec.js");
 
 
 
@@ -33,7 +34,39 @@ const deleteFilePost = asyncHandler(async function(req, res, next) {
 });
 
 
+const deleteFolderPost = asyncHandler(async function(req, res, next) {
+    const files = await getAllFiles(
+        req.body.url,
+        req.body.name,
+        req.user.id,
+    );
+
+    const {data, error} = await supabase.storage
+    .from(process.env.SUPA_BUCKET)
+    .remove(files);
+
+    if (error) {
+        return next(error);
+    }
+
+    await db.deleteFolder({
+        where: {
+            url_name_ownerId: {
+                name: req.body.name,
+                url: req.body.url,
+                ownerId: req.user.id
+            }
+        }
+    });
+
+
+    const redirect = removeFirstDirFromPath(req.body.url);
+    return res.redirect(`/${redirect}`);
+});
+
+
 
 module.exports = {
-    deleteFilePost
+    deleteFilePost,
+    deleteFolderPost
 };
