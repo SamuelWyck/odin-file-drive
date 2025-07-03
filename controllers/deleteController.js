8b2a0a59a6cd4165f9c8d8508f6a74e6ecbc20cc
samuel <samawyck@gmail.com>
2025-07-03 09:41:17 -0500

    Finish file delete handler

--- controllers/deleteController.js
+++ controllers/deleteController.js
@@ -1,10 +1,35 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../db/querys.js");
+const supabase = require("../utils/supabaseConfig.js");
+const path = require("node:path");
+const removeFirstDirFromPath = require("../utils/removeFirstDirFromPath.js");
 
 
 
-const deleteFilePost = asyncHandler(async function(req, res) {
-    console.log(req.body)
+const deleteFilePost = asyncHandler(async function(req, res, next) {
+    const filePath = path.join(req.body.url, req.body.name);
+
+    const {data, error} = await supabase
+        .storage.from(process.env.SUPA_BUCKET)
+        .remove([filePath]);
+    
+    if (error) {
+        return next(error);
+    }
+
+    await db.deleteFile({
+        where: {
+            url_name_ownerId: {
+                name: req.body.name,
+                url: req.body.url,
+                ownerId: req.user.id
+            }
+        }
+    });
+
+
+    const redirect = removeFirstDirFromPath(req.body.url);
+    return res.redirect(`/${redirect}`);
 });
 
 
